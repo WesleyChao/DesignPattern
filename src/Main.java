b44b656f35d467f89ccc537e2f60d8393b6cf038
wesley <caoweixi@icloud.com>
2018-12-18 17:37:42 +0800

    2018-12-18 上班时间截止

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("TestGitHub");
-		 System.out.println("Hello fuck");
+        System.out.println("Hello fuck");
         System.out.println("Hello fuck111");
 
     }
