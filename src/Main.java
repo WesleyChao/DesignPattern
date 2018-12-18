1b2254da5f7d14d0eb3b8bcef85788a317c9c5a4
wesley <caoweixi@icloud.com>
2018-12-18 15:13:48 +0800

    third version

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TestGitHub");
 		 System.out.println("Hello fuck");
+        System.out.println("Hello fuck111");
+
     }
 }
